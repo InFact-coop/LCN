8b4b8d3c2033107d12848dd8d5f5327232004e51
Max Gerber <mgerber94@gmail.com>
2018-02-13 16:01:32 +0000

    ES6'ified https redirect code

--- src/server/app.js
+++ src/server/app.js
@@ -1,16 +1,16 @@
-require("../../config.js");
-const express = require("express");
-const bodyParser = require("body-parser");
-const api_router = require("./routers/api_router");
+require('../../config.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const api_router = require('./routers/api_router');
 
 const app = express();
 
 // force heroku to use https
 
-var https_redirect = function(req, res, next) {
-  if (process.env.NODE_ENV === "production") {
-    if (req.headers["x-forwarded-proto"] != "https") {
-      return res.redirect("https://" + req.headers.host + req.url);
+const https_redirect = (req, res, next) => {
+  if (process.env.NODE_ENV === 'production') {
+    if (req.headers['x-forwarded-proto'] != 'https') {
+      return res.redirect(`https://${req.headers.host}${req.url}`);
     } else {
       return next();
     }
@@ -21,8 +21,8 @@ var https_redirect = function(req, res, next) {
 
 app.use(https_redirect);
 app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.use("/api/v1/", api_router);
+app.use('/api/v1/', api_router);
 
 module.exports = app;
--- src/server/routers/api_router.js
+++ src/server/routers/api_router.js
@@ -1,21 +1,21 @@
-const router = require("express").Router();
-const Airtable = require("airtable");
-const base = Airtable.base(process.env.AIRTABLE_BASE);
-const fs = require("fs");
+const router = require('express').Router();
+const Airtable = require('airtable');
+// const base = Airtable.base(process.env.AIRTABLE_BASE || '0.0.0.0');
+const fs = require('fs');
 
 Airtable.configure({
-  endpointUrl: "https://api.airtable.com",
-  apiKey: process.env.AIRTABLE_API_KEY
+  endpointUrl: 'https://api.airtable.com',
+  apiKey: process.env.AIRTABLE_API_KEY || '1234'
 });
 
-router.route("/upload-form").post((req, res, next) => {
+router.route('/upload-form').post((req, res, next) => {
   let newForm = req.body;
-  base(process.env.AIRTABLE_TABLE).create(newForm, (err, record) => {
+  base(process.env.AIRTABLE_TABLE || 'table').create(newForm, (err, record) => {
     if (err) {
-      console.log("ERR", err);
+      console.log('ERR', err);
       return res.json({ success: false });
     }
-    console.log("SUCCESS", record);
+    console.log('SUCCESS', record);
     return res.json({ success: true });
   });
 });
