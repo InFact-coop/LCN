8b446e07202aa79e273f6d854b54ed527a840037
Max Gerber <mgerber94@gmail.com>
2018-02-13 18:03:19 +0000

    pattern matching to switch navbar to 'dn' on home view #52

--- src/elm/Components/Nav.elm
+++ src/elm/Components/Nav.elm
@@ -2,15 +2,21 @@ module Components.Nav exposing (navBar)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Types exposing (..)
 
 
-navBar : Html msg
-navBar =
-    nav [ class "w-100 h4 bg-white flex flex-row justify-between items-center" ]
-        [ img [ src "/assets/lcn-logo.png", class "h4" ] []
-        , div []
-            [ a [ class "dib mh3 link", href "/#numbers" ] [ text "Add?" ]
-            , a [ class "dib mh3 link", href "/#list-comments" ] [ text "Comments" ]
-            , a [ class "dib mh3 link", href "/#logout" ] [ text "Log out" ]
-            ]
-        ]
+navBar : Model -> Html Msg
+navBar model =
+    case model.view of
+        Home ->
+            nav [ class "dn" ] []
+
+        _ ->
+            nav [ class "w-100 h4 bg-white flex flex-row justify-between items-center" ]
+                [ img [ src "/assets/lcn-logo.png", class "h4" ] []
+                , div []
+                    [ a [ class "dib mh3 link", href "/#numbers" ] [ text "Add?" ]
+                    , a [ class "dib mh3 link", href "/#list-comments" ] [ text "Comments" ]
+                    , a [ class "dib mh3 link", href "/#logout" ] [ text "Log out" ]
+                    ]
+                ]
