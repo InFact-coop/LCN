e0230cf88b3d1dbcdbd3768f3444b4b29139404b
Max Gerber <mgerber94@gmail.com>
2018-04-11 15:30:53 +0100

    + Populating user information on login from database  + Redirecting from Splash Screen depending on this information  + Bomb millisecond splash screen
    Relates #111

--- src/elm/Views/SplashScreen.elm
+++ src/elm/Views/SplashScreen.elm
@@ -7,6 +7,11 @@ import Types exposing (..)
 
 splashScreen : Model -> Html Msg
 splashScreen model =
-    section [ class "flex items-center justify-center" ]
-        [ img [ src "./assets/lcn-logo.png" ] []
+    section [ class "flex items-center justify-center w-100 vh-100" ]
+        [ div
+            [ class "ellipsis" ]
+            [ div [ class "bg-pink w6-l h6-l h5-m w5-m w4 h4 mh4-ns mh3 br-100 dib bouncefade" ] []
+            , div [ class "bg-orange w6-l h6-l h5-m w5-m w4 h4 mh4-ns mh3 br-100 dib bouncefade" ] []
+            , div [ class "bg-green w6-l h6-l h5-m w5-m w4 h4 mh4-ns mh3 br-100 dib bouncefade" ] []
+            ]
         ]
