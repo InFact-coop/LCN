8577e101f97462a66fb806e804711dddd6e3701f
maxgerber <mgerber94@gmail.com>
2018-10-25 11:58:19 +0100

    create promptfont style helper

--- src/elm/Components/StatsThisWeek.elm
+++ src/elm/Components/StatsThisWeek.elm
@@ -1,7 +1,7 @@
 module Components.StatsThisWeek exposing (numericalInput, statsThisWeek)
 
 import Components.LawAreaCheckbox exposing (agencyCheckbox, problemCheckbox)
-import Components.StyleHelpers exposing (bodyFont, classes, displayElement, headlineFont)
+import Components.StyleHelpers exposing (bodyFont, classes, displayElement, headlineFont, promptFont)
 import Helpers exposing (ifThenElse, labelToTuple, onBlurValue, onInputValue, removeSpaces, unionTypeToString)
 import Html exposing (..)
 import Html.Attributes as Attr exposing (..)
@@ -19,9 +19,9 @@ statsThisWeek model =
     section [ class "mb4 mt4" ]
         [ div [ class "flex flex-row mb4 items-baseline" ]
             [ h1 [ classes [ headlineFont ] ] [ text "Your Week" ]
-            , h2 [ classes [ "ml2 fw3" ] ] [ text "(If you don't remember exactly, give us your best estimate)" ]
+            , h2 [ classes [ "ml2", promptFont ] ] [ text "(If you don't remember exactly, give us your best estimate)" ]
             ]
-        , div [ class "grid" ]
+        , div [ class "grid mb4" ]
             [ numericalInput model.peopleSeenWeekly (List.member Triage model.roles || List.member CaseWorker model.roles) peopleSeenText "green" UpdatePeopleSeen
             , numericalInput model.newCasesWeekly (List.member CaseWorker model.roles) "Of these, how many cases were new? (Include returning clients)" "blue" UpdateNewCases
             , numericalInput model.peopleTurnedAwayWeekly (List.member Triage model.roles) "How many enquiries have you had to turn away without signposting anywhere?" "pink" UpdatePeopleTurnedAway
@@ -32,16 +32,16 @@ statsThisWeek model =
             , numericalInput model.lawyerVolunteersWeekly (List.member Management model.roles) "How many of these were lawyer volunteers" "orange" UpdateLawyerVolunteersWeekly
             , numericalInput model.vacanciesWeekly (List.member Management model.roles) "How many vacancies do you have at the moment?" "green" UpdateVacanciesWeekly
             , numericalInput model.mediaCoverageWeekly (List.member Management model.roles) ("Has " ++ unionTypeToString model.lawCentre ++ " Law Centre had any media coverage this week?") "blue" UpdateMediaCoverageWeekly
-            , div [ classes [ "mb4", displayElement (List.member Triage model.roles) ] ]
-                [ label [ for "agencyTypes", classes [ "mb4", bodyFont ] ] [ text "Tick the main types of agencies you referred to this week:" ]
-                , div [ class "mt2" ]
-                    [ agencyCheckbox "Local advice agency"
-                    , agencyCheckbox "Local non-advice support agency"
-                    , agencyCheckbox "National agency/helpline"
-                    , agencyCheckbox "Local MPs"
-                    , agencyCheckbox "Local councillors"
-                    , agencyCheckbox "Other"
-                    ]
+            ]
+        , div [ classes [ "mb4", displayElement (List.member Triage model.roles) ] ]
+            [ label [ for "agencyTypes", classes [ "mb4", headlineFont ] ] [ text "Tick the main types of agencies you referred to this week:" ]
+            , div [ class "mt2" ]
+                [ agencyCheckbox "Local advice agency"
+                , agencyCheckbox "Local non-advice support agency"
+                , agencyCheckbox "National agency/helpline"
+                , agencyCheckbox "Local MPs"
+                , agencyCheckbox "Local councillors"
+                , agencyCheckbox "Other"
                 ]
             ]
         ]
@@ -67,12 +67,9 @@ numericalInput valueFromModel shouldDisplay labelContent thumbColour msg =
         [ div [ class "w-70 flex flex-column justify-between" ]
             [ div [ classes [ bodyFont ] ]
                 [ text <| Tuple.first <| labelToTuple labelContent ]
-            , div [ class "f5 fw3 i mt2" ] [ text <| Tuple.second <| labelToTuple labelContent ]
+            , div
+                [ classes [ promptFont, "mt2" ] ]
+                [ text <| Tuple.second <| labelToTuple labelContent ]
             ]
         , input [ id <| removeSpaces labelContent, type_ "number", value valueString, classes [ "w3 f2 ba b--light-gray pv1 tc mr3", "number-" ++ thumbColour ], Attr.min "0", onBlurValue msg ] []
         ]
-
-
-
--- [ button [ classes [ "b white bn br1 ph2 f4", ifThenElse (unpackedValue == 0) "bg-silver" "bg-blue pointer" ], onClick <| msg decrementedValue ] [ text "-" ]
--- , button [ class "b white bn br1 ph2 f4 bg-orange pointer", onClick <| msg incrementedValue ] [ text "+" ]
